55574ad43eab4589c992cf782de7e97da89d642e
Kimani Walker <kimaniwalker@gmail.com>
2019-01-20 03:14:09 -0600

    Hit weather API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Weather_Url = "http://api.worldweatheronline.com/premium/v1/search.ashx?query=35216&format=json&key=e48d4a5bf0e842209fa90116192001";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+weather:[]
+    };
+  
+  
+  }
+
+  showWeather = async() => {
+    try {
+      let res = await fetch(`${Weather_Url}`);
+      let data = await res.json();
+      console.log(data);
+
+      this.setState({
+        weather: data,
+        
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
+
+  
   render() {
     return (
       <React.Fragment>
-        <div>
+        
           <h1> THE WEATHER APP</h1>
-        </div>
+
+          <button onClick={this.showWeather}>
+            Click Me
+          </button>
+        
       </React.Fragment>
     );
   }
